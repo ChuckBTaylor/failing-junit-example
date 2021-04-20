3a3ad522826055a8d95ef36de5dd0181782b7a31
WillyLS <WLScripps@gmail.com>
2021-04-19 19:15:56 -0700

    Initial commit

---
+++ src/main/java/chuck/junit/exmaple/failingjunit/FailingJunitApplication.java
@@ -0,0 +1,13 @@
+package chuck.junit.exmaple.failingjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FailingJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FailingJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chuck/junit/exmaple/failingjunit/FailingJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package chuck.junit.exmaple.failingjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FailingJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
